dd4aac186746d9a757ddb59f05f3b2b751b1cf2f
danmalcolm <danmalc@gmail.com>
2016-10-23 14:22:48 +0100

    Further test to ensure logic for simplest case still works

--- src/CheckoutKata.Tests/CheckoutCalculatorTests.cs
+++ src/CheckoutKata.Tests/CheckoutCalculatorTests.cs
@@ -16,5 +16,18 @@ namespace CheckoutKata.Tests
 
             calculator.TotalSkuPrice.Should().Be(10m);
         }
+
+        [Fact]
+        public void should_calculate_total_cost_for_same_sku_added_multiple_times()
+        {
+            var repository = new TestSkuRepository();
+            repository.Add(new Sku("A", 10m));
+            var calculator = new CheckoutCalculator(repository);
+
+            calculator.AddSku("A");
+            calculator.AddSku("A");
+
+            calculator.TotalSkuPrice.Should().Be(20m);
+        }
     }
 }
