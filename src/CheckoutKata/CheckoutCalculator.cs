c86e85aa4c2991c7d32e1fb1a3800ace52f35135
danmalcolm <danmalc@gmail.com>
2016-10-23 14:21:33 +0100

    Simplest thing implementation to pass first test

--- src/CheckoutKata/CheckoutCalculator.cs
+++ src/CheckoutKata/CheckoutCalculator.cs
@@ -1,17 +1,22 @@
-﻿namespace CheckoutKata
+﻿using System.Collections.Generic;
+
+namespace CheckoutKata
 {
     public class CheckoutCalculator
     {
+        private readonly ISkuRepository repository;
+
         public CheckoutCalculator(ISkuRepository repository)
         {
-            
+            this.repository = repository;
         }
 
         public void AddSku(string skuCode)
         {
-            
+            var sku = repository.FindByCode(skuCode);
+            TotalSkuPrice += sku.UnitPrice;
         }
 
-        public decimal TotalSkuPrice => 10m;
+        public decimal TotalSkuPrice { get; private set; }
     }
 }
