d8c3a100997c51218b6e5444ed0340e770b2dcf3
danmalcolm <danmalc@gmail.com>
2016-10-23 14:01:41 +0100

    First outline test. Doesn't even compile, but this is a good way of exploring "shape" of the implementation. Writing a test first gets us thinking about how the client UI code will interact with our system.

---
+++ src/CheckoutKata.Tests/CheckoutCalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CheckoutKata.Tests
+{
+    public class CheckoutCalculatorTests
+    {
+        [Fact]
+        public void should_calculate_total_cost_for_single_sku()
+        {
+            var productRepository = new TestProductRepository();
+            productRepository.Add(new Sku("A", 10m));
+            var calculator = new CheckoutCalculator(productRepository);
+
+            calculator.AddSku("A");
+
+            calculator.TotalSkuPrice.Should.Be(10m);
+        }
+    }
+}
